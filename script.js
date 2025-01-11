ca083005ea71ea4da6184ed1378f82b83eb6ab3a
Luciano Alfredo <lucianalfred60@gmail.com>
2025-01-11 11:38:50 +0100

    script finished

---
+++ script.js
@@ -0,0 +1,21 @@
+function updateClock(){
+
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2,'0');
+    const minutes = now.getMinutes().toString().padStart(2,'0');
+    const seconds = now.getSeconds().toString().padStart(2,'0');
+    const day = now.getDate().toString().padStart(2, '0');
+    const month = (now.getMonth()+1).toString().padStart(2,'0');
+    const year = now.getFullYear();
+
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    const dateString = `${day}/${month}/${year}`;
+
+    document.getElementById('time').textContent = timeString;
+    document.getElementById('date').textContent = dateString;
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
