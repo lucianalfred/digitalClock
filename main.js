367882b32966cbf7d7f637bc1da23351f7da0492
Luciano Alfredo <lucianalfred60@gmail.com>
2025-01-11 12:29:10 +0100

    main file update

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
-const { app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 let mainWindow;
 
-app.on('ready', () =>{
-    mainWindow = new BrowserWindow({
-        width: 400,
-        height: 300,
-        resizable:false,
-        webPreferences: {
-            preload:path.join(__dirname, 'src/script.js'),
-            nodeIntegration: true
-        }
-    });
-});
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 400,
+    height: 300,
+    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'src/script.js'),
+    },
+  });
 
-mainWindow.loadFile('src/index.html');
-
-mainWindow.setMenu(null);
-
-mainWindow.on('closed', () => {
+  mainWindow.loadFile(path.join(__dirname, 'src/index.html'));
+  
+  // Remover o menu padrão (opcional)
+  mainWindow.setMenu(null);
+  
+  mainWindow.on('closed', () => {
     mainWindow = null;
+  });
 });
 
-
-app.on('window-all-closed', ()=>{
-    if(process.platform !== 'darwin'){
-        app.quit();
-    }
+// Sair quando todas as janelas forem fechadas
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
-app.on('activate', ()=>{
-    if(BrowserWindow.getAllWindows().length === 0){
-        createMainWindow();
-    }
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
 });
